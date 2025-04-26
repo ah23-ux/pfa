d5278528663724e8ac34809d1cea0327b30e6d7c
HAKKOU AYA <hakkouaya@new-host-2.home>
2025-04-26 15:50:58 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}> 
+      <Text style={styles.title}> Welcome</Text>
+      <Text style={{marginTop: 10, marginBottom: 10}}>marwa yallah ndroppiw out</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title:{
+        fontWeight: 'bold',
+        fontSize: 18
+
+    }
+})
