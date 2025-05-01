1ccdc11fb84a369829ec5ab5979df89bec636600
ah23-ux <ayahakkou@gmail.com>
2025-05-01 21:48:21 +0100

    Mes modifications sur profil.jsx

--- app/profil.jsx
+++ app/profil.jsx
@@ -1,16 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import{Link} from 'expo-router'
 
-const About = () => {
-  return (
-    <View>
-      <Text>about Page</Text>
-      <Link href="/">Home page</Link>
+
+const profil = () => {
+  return(
+    <View style={styles.container}>
+       <Text style={styles.up}> Votre profile </Text>
+      <View style={styles.info}> 
+      <Pressable style={({pressed})=>[styles.btn,pressed && styles.pressed]}><Text style={{color:'black', fontFamily: 'monospace'}}>email</Text></Pressable>
+      </View>
+      <View style={styles.info}>
+        <Pressable style={({pressed})=>[styles.btn,pressed && styles.pressed]}><Text style={{color:'black', fontFamily: 'monospace'}}>telephone</Text></Pressable>
+      </View>
+      <View style={styles.info}>
+      <Pressable style={({pressed})=>[styles.btn,pressed && styles.pressed]}><Text style={{color:'black', fontFamily: 'monospace'}}>localisation</Text></Pressable>
+      </View>
     </View>
   )
+  
 }
 
-export default About
+export default profil
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'hsla(132, 94.90%, 30.80%, 0.75)',
+  },
+  up: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 10,
+  },
+  btn:{
+    backgroundColor:'#f4b700',
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed:{
+    opacity:0.8
+  }
+})
